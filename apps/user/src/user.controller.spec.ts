3fbea51c13e2b6d6c5cd57fedb0e663ed196be9c
hugoradiawan <hugoradiawan@gmail.com>
2023-12-25 20:06:50 +0700

    test: user register

--- apps/user/src/user.controller.spec.ts
+++ apps/user/src/user.controller.spec.ts
@@ -1,22 +1,84 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { UserController } from './user.controller';
-// import { UserService } from './user.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { SharedService } from '@app/shared';
+import { RmqData } from '@app/shared/interfaces/rmq-data.interface';
+import { EmailAndUsernameDto } from '@app/shared/dto/emailAndUsername.dto';
+import { Channel, Message } from 'amqplib';
+import { RabbitMQMessage } from '@app/shared/dto/rmq-event.dto';
+import { User } from '@app/shared/interfaces/user.interface';
+import { RmqContext } from '@nestjs/microservices';
+import { Model } from 'mongoose';
+import { getModelToken } from '@nestjs/mongoose';
 
-// describe('UserController', () => {
-//   let userController: UserController;
+describe('UserController', () => {
+  let userService: UserService;
+  let sharedService: SharedService;
+  let userController: UserController;
+  let userModel: Model<User>;
 
-//   beforeEach(async () => {
-//     const app: TestingModule = await Test.createTestingModule({
-//       controllers: [UserController],
-//       providers: [UserService],
-//     }).compile();
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [
+        UserService,
+        SharedService,
+        {
+          provide: getModelToken('User'),
+          useValue: {
+            findOne: jest.fn().mockResolvedValue({
+              id: 1,
+              email: 'test@test.com',
+              username: 'test',
+            }),
+          },
+        },
+        {
+          provide: getModelToken('Profile'),
+          useValue: {},
+        },
+        {
+          provide: getModelToken('ZodiacEnd'),
+          useValue: {},
+        },
+      ],
+    }).compile();
 
-//     userController = app.get<UserController>(UserController);
-//   });
+    userService = app.get<UserService>(UserService);
+    sharedService = app.get<SharedService>(SharedService);
+    userController = app.get<UserController>(UserController);
+    userModel = app.get<Model<User>>(getModelToken('User'));
+  });
 
-//   describe('root', () => {
-//     it('should return "Hello World!"', () => {
-//       expect(userController.getHello()).toBe('Hello World!');
-//     });
-//   });
-// });
+  describe('register', () => {
+    it('should return user when findOneByEmailOrByUsername returns a user', async () => {
+      const mockUser = {
+        id: 1,
+        email: 'test@test.com',
+        username: 'test',
+      } as User;
+      const mockExtractData = {
+        ack: jest.fn(),
+        nack: jest.fn(),
+        channel: {} as Channel,
+        message: {} as Message,
+        event: {
+          data: mockUser,
+          pattern: 'find-user-by-email-or-username',
+          id: '1',
+        } as RabbitMQMessage<EmailAndUsernameDto>,
+      } as RmqData<EmailAndUsernameDto>;
+      const mockedContext = {} as RmqContext;
+
+      jest.spyOn(sharedService, 'extractData').mockReturnValue(mockExtractData);
+      jest
+        .spyOn(userService, 'findOneByEmailOrByUsername')
+        .mockResolvedValue(mockUser);
+      const userModelSave = jest.fn().mockResolvedValue(mockUser);
+      userModel.findOne = userModelSave;
+
+      expect(await userController.register(mockedContext)).toBe(mockUser);
+      expect(mockExtractData.ack).toHaveBeenCalled();
+    });
+  });
+});
